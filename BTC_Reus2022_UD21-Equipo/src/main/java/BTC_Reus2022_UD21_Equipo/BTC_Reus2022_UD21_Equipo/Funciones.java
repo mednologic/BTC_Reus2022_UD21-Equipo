d0c5b28c62d9dd56aaf3d46a336a7b6829b23f78
mednologic <mednologic@gmail.com>
2022-05-09 12:33:38 +0200

    Update Funciones.java

--- BTC_Reus2022_UD21-Equipo/src/main/java/BTC_Reus2022_UD21_Equipo/BTC_Reus2022_UD21_Equipo/Funciones.java
+++ BTC_Reus2022_UD21-Equipo/src/main/java/BTC_Reus2022_UD21_Equipo/BTC_Reus2022_UD21_Equipo/Funciones.java
@@ -1,5 +1,14 @@
 package BTC_Reus2022_UD21_Equipo.BTC_Reus2022_UD21_Equipo;
 
 public class Funciones {
+	
+	int valorInput;
+	
+	public String tipoMoneda() {
+		String tipoMoneda;
+		
+		return tipoMoneda;
+		
+	}
 
 }
