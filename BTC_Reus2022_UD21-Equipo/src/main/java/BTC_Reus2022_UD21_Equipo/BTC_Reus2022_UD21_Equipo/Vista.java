2e40c04eac30983b31b22449676b12b4a6178e34
OctavioBernalGH <103035621+OctavioBernalGH@users.noreply.github.com>
2022-05-09 13:28:20 +0200

    Se modifica la vista.
    Se definen componentes y etiquetas de ellos.
    Se añaden comentarios.
    Se reestructura el código de la vista.

--- BTC_Reus2022_UD21-Equipo/src/main/java/BTC_Reus2022_UD21_Equipo/BTC_Reus2022_UD21_Equipo/Vista.java
+++ BTC_Reus2022_UD21-Equipo/src/main/java/BTC_Reus2022_UD21_Equipo/BTC_Reus2022_UD21_Equipo/Vista.java
@@ -1,22 +1,43 @@
 package BTC_Reus2022_UD21_Equipo.BTC_Reus2022_UD21_Equipo;
 
+/**
+ * @author Josep Oriol López Bosch 
+ * @author David Dalmau Dieguez
+ * @author Octavio Bernal Vilana
+ * 
+ * @version 0.0.1
+ * @date 09/05/2022
+ */
+
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
+import javax.swing.UIManager;
+import java.awt.Color;
+import javax.swing.JButton;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
 
+/**
+ * Este programa realizará las diferentes conversiones de divisas, mediante dos
+ * comboBox se eligirá el tipo de moneda a convertir.
+ */
 public class Vista {
 
-	private JFrame frame;
+	// Atributos de clase.
+	private JFrame frmConversorDeDivisas;
+	private JTextField moneda_arriba;
+	private JTextField moneda_abajo;
+	private JTextField txtMoneda;
 
-	/**
-	 * Launch the application.
-	 */
+	// Se crea una instancia de la vista y se otorga visibilidad en el JFrame.
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					Vista window = new Vista();
-					window.frame.setVisible(true);
+					window.frmConversorDeDivisas.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -24,20 +45,190 @@ public class Vista {
 		});
 	}
 
-	/**
-	 * Create the application.
-	 */
+	// Llama al método que ejecuta la vista.
 	public Vista() {
 		initialize();
 	}
 
 	/**
-	 * Initialize the contents of the frame.
+	 * Se inicializa la vista, se cargan los componentes, se define su forma y se
+	 * define su posición en el JFrame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+		// Se crea una instancia de la clase JFrame y se definen sus atributos.
+		frmConversorDeDivisas = new JFrame();
+		frmConversorDeDivisas.setTitle("Conversor de divisas");
+		frmConversorDeDivisas.getContentPane().setBackground(Color.DARK_GRAY);
+		frmConversorDeDivisas.setBackground(UIManager.getColor("Button.darkShadow"));
+		frmConversorDeDivisas.setBounds(100, 100, 663, 585);
+		frmConversorDeDivisas.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmConversorDeDivisas.getContentPane().setLayout(null);
 
+		// Se crea componente botón y se asignan atributos.
+		JButton btn_num9 = new JButton("9");
+		btn_num9.setBorder(null);
+		btn_num9.setForeground(Color.BLACK);
+		btn_num9.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btn_num9.setBackground(Color.LIGHT_GRAY);
+		btn_num9.setBounds(518, 171, 100, 75);
+		frmConversorDeDivisas.getContentPane().add(btn_num9);
+
+		// Se crea componente botón y se asignan atributos.
+		JButton btn_num8 = new JButton("8");
+		btn_num8.setBorder(null);
+		btn_num8.setForeground(Color.BLACK);
+		btn_num8.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btn_num8.setBackground(Color.LIGHT_GRAY);
+		btn_num8.setBounds(408, 171, 100, 75);
+		frmConversorDeDivisas.getContentPane().add(btn_num8);
+
+		// Se crea componente botón y se asignan atributos.
+		JButton btn_num7 = new JButton("7");
+		btn_num7.setBorder(null);
+		btn_num7.setForeground(Color.BLACK);
+		btn_num7.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btn_num7.setBackground(Color.LIGHT_GRAY);
+		btn_num7.setBounds(298, 171, 100, 75);
+		frmConversorDeDivisas.getContentPane().add(btn_num7);
+
+		// Se crea componente botón y se asignan atributos.
+		JButton btn_num6 = new JButton("6");
+		btn_num6.setBorder(null);
+		btn_num6.setForeground(Color.BLACK);
+		btn_num6.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btn_num6.setBackground(Color.LIGHT_GRAY);
+		btn_num6.setBounds(518, 257, 100, 75);
+		frmConversorDeDivisas.getContentPane().add(btn_num6);
+
+		// Se crea componente botón y se asignan atributos.
+		JButton btn_num5 = new JButton("5");
+		btn_num5.setBorder(null);
+		btn_num5.setForeground(Color.BLACK);
+		btn_num5.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btn_num5.setBackground(Color.LIGHT_GRAY);
+		btn_num5.setBounds(408, 257, 100, 75);
+		frmConversorDeDivisas.getContentPane().add(btn_num5);
+
+		// Se crea componente botón y se asignan atributos.
+		JButton btn_num4 = new JButton("4");
+		btn_num4.setBorder(null);
+		btn_num4.setForeground(Color.BLACK);
+		btn_num4.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btn_num4.setBackground(Color.LIGHT_GRAY);
+		btn_num4.setBounds(298, 257, 100, 75);
+		frmConversorDeDivisas.getContentPane().add(btn_num4);
+
+		// Se crea componente botón y se asignan atributos.
+		JButton btn_num3 = new JButton("3");
+		btn_num3.setBorder(null);
+		btn_num3.setForeground(Color.BLACK);
+		btn_num3.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btn_num3.setBackground(Color.LIGHT_GRAY);
+		btn_num3.setBounds(518, 343, 100, 75);
+		frmConversorDeDivisas.getContentPane().add(btn_num3);
+
+		// Se crea componente botón y se asignan atributos.
+		JButton btn_num2 = new JButton("2");
+		btn_num2.setBorder(null);
+		btn_num2.setForeground(Color.BLACK);
+		btn_num2.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btn_num2.setBackground(Color.LIGHT_GRAY);
+		btn_num2.setBounds(408, 343, 100, 75);
+		frmConversorDeDivisas.getContentPane().add(btn_num2);
+
+		// Se crea componente botón y se asignan atributos.
+		JButton btn_num1 = new JButton("1");
+		btn_num1.setBorder(null);
+		btn_num1.setForeground(Color.BLACK);
+		btn_num1.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btn_num1.setBackground(Color.LIGHT_GRAY);
+		btn_num1.setBounds(298, 343, 100, 75);
+		frmConversorDeDivisas.getContentPane().add(btn_num1);
+
+		// Se crea componente botón y se asignan atributos.
+		JButton btn_num0 = new JButton("0");
+		btn_num0.setBorder(null);
+		btn_num0.setForeground(Color.BLACK);
+		btn_num0.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btn_num0.setBackground(Color.LIGHT_GRAY);
+		btn_num0.setBounds(408, 430, 100, 75);
+		frmConversorDeDivisas.getContentPane().add(btn_num0);
+
+		// Se crea componente botón y se asignan atributos.
+		JButton btn_ce = new JButton("CE");
+		btn_ce.setBorder(null);
+		btn_ce.setForeground(Color.WHITE);
+		btn_ce.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btn_ce.setBackground(Color.GRAY);
+		btn_ce.setBounds(408, 78, 100, 75);
+		frmConversorDeDivisas.getContentPane().add(btn_ce);
+
+		// Se crea componente botón y se asignan atributos.
+		JButton btn_borrarNumero = new JButton("del");
+		btn_borrarNumero.setBorder(null);
+		btn_borrarNumero.setForeground(Color.WHITE);
+		btn_borrarNumero.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btn_borrarNumero.setBackground(Color.GRAY);
+		btn_borrarNumero.setBounds(518, 78, 100, 75);
+		frmConversorDeDivisas.getContentPane().add(btn_borrarNumero);
+
+		// Se crea componente botón y se asignan atributos.
+		JButton btn_coma = new JButton(",");
+		btn_coma.setBorder(null);
+		btn_coma.setForeground(Color.BLACK);
+		btn_coma.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btn_coma.setBackground(Color.LIGHT_GRAY);
+		btn_coma.setBounds(518, 429, 100, 75);
+		frmConversorDeDivisas.getContentPane().add(btn_coma);
+
+		// Se crea el campo de texto y asigan atributos.
+		moneda_arriba = new JTextField();
+		moneda_arriba.setForeground(Color.WHITE);
+		moneda_arriba.setBorder(null);
+		moneda_arriba.setEditable(false);
+		moneda_arriba.setBackground(Color.DARK_GRAY);
+		moneda_arriba.setBounds(20, 173, 226, 73);
+		frmConversorDeDivisas.getContentPane().add(moneda_arriba);
+		moneda_arriba.setColumns(10);
+
+		// Se crea el campo de texto y asigan atributos.
+		moneda_abajo = new JTextField();
+		moneda_abajo.setForeground(Color.WHITE);
+		moneda_abajo.setBorder(null);
+		moneda_abajo.setEditable(false);
+		moneda_abajo.setColumns(10);
+		moneda_abajo.setBackground(Color.DARK_GRAY);
+		moneda_abajo.setBounds(20, 345, 226, 73);
+		frmConversorDeDivisas.getContentPane().add(moneda_abajo);
+
+		// Se crea el campo de texto y asigan atributos.
+		txtMoneda = new JTextField();
+		txtMoneda.setForeground(Color.WHITE);
+		txtMoneda.setFont(new Font("Tahoma", Font.BOLD, 16));
+		txtMoneda.setText("Moneda");
+		txtMoneda.setEditable(false);
+		txtMoneda.setColumns(10);
+		txtMoneda.setBorder(null);
+		txtMoneda.setBackground(Color.DARK_GRAY);
+		txtMoneda.setBounds(20, 11, 162, 73);
+		frmConversorDeDivisas.getContentPane().add(txtMoneda);
+
+		// Se crea el comboBox, se definen sus atributos y valores.
+		JComboBox comboBox_superior = new JComboBox();
+		comboBox_superior.setBorder(null);
+		comboBox_superior.setModel(new DefaultComboBoxModel(new String[] { "Dolar", "Euro", "Rubla" }));
+		comboBox_superior.setBackground(Color.DARK_GRAY);
+		comboBox_superior.setForeground(Color.WHITE);
+		comboBox_superior.setBounds(20, 257, 226, 22);
+		frmConversorDeDivisas.getContentPane().add(comboBox_superior);
+
+		// Se crea el comboBox, se definen sus atributos y valores.
+		JComboBox comboBox_inferior = new JComboBox();
+		comboBox_inferior.setModel(new DefaultComboBoxModel(new String[] { "Dolar", "Euro", "Rubla" }));
+		comboBox_inferior.setForeground(Color.WHITE);
+		comboBox_inferior.setBorder(null);
+		comboBox_inferior.setBackground(Color.DARK_GRAY);
+		comboBox_inferior.setBounds(20, 430, 226, 22);
+		frmConversorDeDivisas.getContentPane().add(comboBox_inferior);
+	}
 }
