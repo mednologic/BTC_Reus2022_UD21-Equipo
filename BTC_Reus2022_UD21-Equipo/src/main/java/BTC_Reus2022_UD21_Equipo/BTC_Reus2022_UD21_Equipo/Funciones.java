1b26158ff8333295238d9b69e323809b49f11614
mednologic <mednologic@gmail.com>
2022-05-09 12:49:12 +0200

    All conversions functions

--- BTC_Reus2022_UD21-Equipo/src/main/java/BTC_Reus2022_UD21_Equipo/BTC_Reus2022_UD21_Equipo/Funciones.java
+++ BTC_Reus2022_UD21-Equipo/src/main/java/BTC_Reus2022_UD21_Equipo/BTC_Reus2022_UD21_Equipo/Funciones.java
@@ -32,6 +32,26 @@ public class Funciones {
 		retorno = inputValue*1.05;
 		return retorno;
 	}
+	public double DollarEuro(double inputValue) {
+		double retorno=0.0;
+		retorno = inputValue/1.05;
+		return retorno;
+	}
+	public double DollarRuble(double inputValue) {
+		double retorno=0.0;
+		retorno = inputValue*69.35;
+		return retorno;
+	}
+	public double RubleEuro(double inputValue) {
+		double retorno=0.0;
+		retorno = inputValue/0.014;
+		return retorno;
+	}
+	public double RubleDollar(double inputValue) {
+		double retorno=0.0;
+		retorno = inputValue/0.013;
+		return retorno;
+	}
 
 
 }
