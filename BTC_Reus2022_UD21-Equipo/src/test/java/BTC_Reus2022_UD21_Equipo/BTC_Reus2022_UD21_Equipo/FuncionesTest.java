2136256adac3e285f41967783d81ddc162b52e84
DavidDalmauDieguez <david.dieguez13@gmail.com>
2022-05-09 12:42:21 +0200

    Commit con JUnit de funcion tipoMonedas

--- BTC_Reus2022_UD21-Equipo/src/test/java/BTC_Reus2022_UD21_Equipo/BTC_Reus2022_UD21_Equipo/FuncionesTest.java
+++ BTC_Reus2022_UD21-Equipo/src/test/java/BTC_Reus2022_UD21_Equipo/BTC_Reus2022_UD21_Equipo/FuncionesTest.java
@@ -7,8 +7,17 @@ import org.junit.jupiter.api.Test;
 class FuncionesTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testTipoMoneda() {
+		Funciones FuncionTest1 = new Funciones();
+		String esperado = "Euro";
+		assertEquals(esperado, FuncionTest1.tipoMoneda(1));
+		
+		Funciones FuncionTest2 = new Funciones();
+		String esperado2 = "Rubla";
+		assertEquals(esperado2, FuncionTest2.tipoMoneda(2));
+		
+		Funciones FuncionTest3 = new Funciones();
+		String esperado3 = "Dollar";
+		assertEquals(esperado3, FuncionTest3.tipoMoneda(3));
 	}
-
 }
